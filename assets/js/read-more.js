ef29173d9731957884cf7f237ece1273cefe6ecf
shivani743 <53550692+shivani743@users.noreply.github.com>
2021-03-02 17:06:24 +0530

    eighty-ninee

---
+++ assets/js/read-more.js
@@ -0,0 +1,13 @@
+< script >
+    jQuery(document).ready(function() {
+
+        jQuery('.more').readmore({
+            speed: 300,
+            collapsedHeight: 140,
+            moreLink: '<a href="#">Read more &gt;</a>',
+            lessLink: '<a href="#">Read less</a>',
+            heightMargin: 16
+        });
+
+    }); <
+/script>
